eedd8a2e531f7229ef58c25ab1f37d2cddd760e4
AkasbiYasser <yasserakasbi.07@gmail.com>
2024-08-12 22:13:02 -0400

    ip-pub

--- scrum-frontend/src/redux/actions/authActions.js
+++ scrum-frontend/src/redux/actions/authActions.js
@@ -3,7 +3,7 @@ import { LOGIN_SUCCESS, REGISTER_SUCCESS, REGISTER_FAILURE } from './types';
 
 export const login = (credentials) => async (dispatch) => {
   try {
-    const res = await axios.post('http://scrum-backend-service:8080/api/auth/register', credentials);
+    const res = await axios.post('http://20.164.56.175:8080/api/auth/register', credentials);
     const { token, roles, userId } = res.data; // Récupérez l'ID
     const userRole = roles[0].name;
 
